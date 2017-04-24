7c413505e0296047a7b601c56b4b305d0a9aeb9b
liujin <liujin@115_159_79_60.(none)>
2017-04-24 16:33:51 +0800

    4

---
+++ view/cfg/base.cfg.php
@@ -0,0 +1,13 @@
+<?php
+//定义根路径
+	$basedir=str_replace('\\','/',dirname(dirname(__FILE__))).'/';
+	define(BASE_DIR,$basedir);
+	unset($basedir);
+//定义常数参数
+	define(CONF_DIR,'cfg/');
+	define(INC_DIR,'inc/');
+	define(CSS_DIR,'css/');
+	define(INC_IDX,'idx.inc.php');
+//加载页面
+	require_once(BASE_DIR.INC_DIR.INC_IDX);
+?>
