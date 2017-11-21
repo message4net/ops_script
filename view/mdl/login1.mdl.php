ab5203003022309687e6c3c098b681c79443e716
范永迪 <message4net@outlook.com>
2017-11-21 23:57:19 +0800

    modified:   ../../weather/mdl/modify.mdl.php

---
+++ view/mdl/login1.mdl.php
@@ -0,0 +1,11 @@
+<?php session_start();
+
+//$returnarr[0]=array(str_replace('\\','/',dirname(dirname(__FILE__))).'/cfg/base.cfg.php');
+
+$returnarr[0]=array('aaaaaaaaab');
+echo json_encode($returnarr);
+
+
+
+//require_once str_replace('\\','/',dirname(dirname(__FILE__))).'/cfg/base.cfg.php';
+?>
